90422be69e9409d829999cd4029047ed1814ddfe
stephenyoud <syoud@mail.csuchico.edu>
2021-05-17 09:29:32 -0700

    Fixed continuous input by adding a timeout option

--- scapy/driver.py
+++ scapy/driver.py
@@ -2,11 +2,11 @@ from scapy.all import *
 import argparse
 from entropy_computer import Entropy_Computer
 
-def callback(ec, packet):
-    if IP in packet:
-        ec.process_packet(packet)
+def run(ec, pcap = None, t = 10):
+    def callback(packet):
+        if IP in packet:
+            ec.process_packet(packet)
 
-def run(ec, pcap = None):
     if pcap is not None:
         # If there is an input pcap, we run our entropy computer on that
         if not os.path.exists(os.path.join(os.getcwd(), 'pcap_files', pcap)):
@@ -15,22 +15,23 @@ def run(ec, pcap = None):
             
         # Reads pcap and runs 'callback' on each packet
         for packet in rdpcap(os.path.join(os.getcwd(), 'pcap_files', pcap)):
-            callback(ec, packet)
+            callback(packet)
     else:
         # If there is no input pcap, we sniff with a stream of incoming packets
-        sniff(filter='ip', prn=callback)
+        sniff(filter='ip', prn=callback, timeout=t)
 
 if __name__ == '__main__':
     # Get command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', help = 'Input File')
+    parser.add_argument('-t', '--timeout', help = 'Timeout Length (for sniffing)')
     args = parser.parse_args()
 
     # Define entropy computer
     ec = Entropy_Computer()
     
     # Pcap is either live (sniffing) or pre-recorded
-    run(ec = ec, pcap = args.input)
+    run(ec = ec, pcap = args.input, t = int(args.timeout))
 
     # Print out the stats obtianed
     stats = ec.get_stats()
